7e3f669f358f1697289d1457c23fb953c3cecf9c
sacultang <sacultang@gmail.com>
2022-04-30 21:12:53 +0900

    what single quotes?

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import "./App.css";
-import Header from "./Header";
+import './App.css'
+import Header from './Header'
 
 function App() {
   return (
-    <div className="App">
-      <Header title="Hello"></Header>
+    <div className='App'>
+      <Header title='Hello'></Header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
