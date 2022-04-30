3764d47abdf26a25e6d554397bc0298f02b05df7
sacultang <sacultang@gmail.com>
2022-04-30 21:17:01 +0900

    quotes

--- src/Header.js
+++ src/Header.js
@@ -24,7 +24,7 @@ function Nav(props) {
 }
 function Header(props) {
   const topics = [
-    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: 'css', body: 'css is ...' },
     { id: 3, title: 'js', body: 'js is ...' },
   ]
