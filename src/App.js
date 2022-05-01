86221f59701f042dbaee74e80a79086f72bd9588
sacultang <sacultang@gmail.com>
2022-05-01 20:03:16 +0900

    feat : create react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
-import Header from './Header'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 function App() {
   return (
-    <div className='App'>
-      <Header title='Hello'></Header>
-    </div>
+    <BrowserRouter>
+      <Route path='/' exact component={Home}></Route>
+      <Route path='/profile' exact component={Profile}></Route>
+      <Route path='/profile/:id' component={Profile}></Route>
+      <Route path='/about' component={About}></Route>
+    </BrowserRouter>
   )
 }
 
